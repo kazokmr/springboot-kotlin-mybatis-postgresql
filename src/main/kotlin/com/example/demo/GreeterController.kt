d42511c9b8acaa6ca2f66c35a583772e60ceaa91
Kazuhiko Okumura <engkokmr@gmail.com>
2021-06-05 21:10:03 +0900

    DI の実装

--- src/main/kotlin/com/example/demo/GreeterController.kt
+++ src/main/kotlin/com/example/demo/GreeterController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("greeter")
-class GreeterController {
+class GreeterController(private val greeter: Greeter) {
 
     @GetMapping("/hello")
     fun hello(@RequestParam("name") name: String): HelloResponse {
@@ -25,4 +25,9 @@ class GreeterController {
     @PostMapping("/hello")
     fun helloByPost(@RequestBody request: HelloRequest) = HelloResponse("Hello ${request.name}")
 
+    @GetMapping("/hello/byservice/{name}")
+    fun helloByService(@PathVariable("name") name: String): HelloResponse {
+        val message = greeter.sayHello(name)
+        return HelloResponse(message)
+    }
 }
